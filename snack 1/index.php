0efe1ecc884a0c1270fc8d8c71996788fb9e9407
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-15 12:18:49 +0200

    Add the sctructure and the Arrays for the Home Teams and the Guest Teams

---
+++ snack 1/index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $homeTeams = 
+    [
+        "homeTeam" => "Dolomiti Energia Trentino", "Carpegna Prosciutto Pesaro", "NutriBullet Treviso Basket", "UNAHOTELS Reggio Emilia", "Virtus Segafredo Bologna", "Germani Brescia", "Generazione Vincente Napoli Basket", "Givona Scafati Basket",
+        "homePoints" => "83", "89", "89", "74", "93", "90", "88", "102",
+    ];
+
+    $guestTeams = 
+    [
+        "guestTeam" => "Bertram Berthona Tortona", "Estra Pistoia", "EA7 Emporio Armani Milano", "Happy Casa Brindisi", "Vanoli Basket Cremona", "Umana Reyez Venezia", "Banco di Sardegna Sassari", "Openjobmetis Varese",
+        "guestPoints" => "81", "82", "91", "66", "85", "84", "79", "90", 
+    ];
+
+
+    var_dump($homeTeams);
+    var_dump($guestTeams);
+    // ciclo for per far ciclare all'interno degli array i nomi delle squadre di casa e quelle ospiti
+    // ciclo for per far ciclare all'interno degli array per i punti
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    <h1>Calendario partire di Basket:</h1>
+    <?= ?>
+</body>
+</html>
